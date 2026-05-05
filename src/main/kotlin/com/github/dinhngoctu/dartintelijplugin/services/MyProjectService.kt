b7d591e34f4cd31278d220d50bb674508d110ea8
GitHub Action <action@github.com>
2026-05-05 10:02:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/dinhngoctu/dartintelijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dinhngoctu/dartintelijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dinhngoctu.dartintelijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dinhngoctu.dartintelijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
